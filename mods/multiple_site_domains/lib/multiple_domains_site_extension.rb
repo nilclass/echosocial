3132317a0360737da1997c13a1174787126f35c1
niklas <niklas@brueckenschlaeger.de>
2010-02-08 18:04:28 +0100

    Site.for_domain now working (with a subselect, but actually working), test is in the GREEN again then :)

--- mods/multiple_site_domains/lib/multiple_domains_site_extension.rb
+++ mods/multiple_site_domains/lib/multiple_domains_site_extension.rb
@@ -4,11 +4,11 @@ module MultipleDomainsSiteExtension
       has_many :site_domains
 
       named_scope :for_domain, lambda { |domain|
-        domain_query = '(sites.domain = ? OR site_domains.domain = ?)'
+        # I tried to use a INNER JOIN instead, but it would return an invalid site record (referencing the SiteDomain's id instead of the one of the Site)
+        domain_query = '(sites.domain = ? OR sites.id IN (SELECT site_id FROM site_domains WHERE site_domains.domain = ?))'
         { :conditions => (Conf.dynamic_sites ?
                           ["#{domain_query} AND sites.enabled = ?", domain, domain, true] :
-                          ["#{domain_query} AND sites.id IN (?)", domain, domain, Conf.enabled_site_ids]),
-          :joins => :site_domains
+                          ["#{domain_query} AND sites.id IN (?)", domain, domain, Conf.enabled_site_ids])
         }
       }
     end
--- mods/multiple_site_domains/test/unit/site_domains_test.rb
+++ mods/multiple_site_domains/test/unit/site_domains_test.rb
@@ -7,17 +7,15 @@ class SiteDomainsTest < ActiveSupport::TestCase
   def setup
     SiteDomain.all.map(&:destroy)
     @domains = []
-    @domains << SiteDomain.create!(:domain => 'multiple-test.localhost', :site_id => Site.all[0])
-    @domains << SiteDomain.create!(:domain => 'xyz.localhost', :site_id => Site.all[0])
-    @domains << SiteDomain.create!(:domain => 'test.example', :site_id => Site.all[1])
-    $stderr.puts("ALL SITES:\n"+Site.all.inspect)
+    @domains << SiteDomain.create!(:domain => 'multiple-test.localhost', :site_id => Site.find(1).id)
+    @domains << SiteDomain.create!(:domain => 'xyz.localhost', :site_id => Site.find(1).id)
+    @domains << SiteDomain.create!(:domain => 'test.example', :site_id => Site.find(2).id)
   end
 
   def test_should_find_sites
     @domains.each do |domain|
-      site = Site.for_domain(domain.domain).first
-      $stderr.puts "site for #{domain.domain} is #{site.inspect}"
-      assert site == domain.site
+      assert (site = Site.for_domain(domain.domain).first) == domain.site
+      assert site.domains.include?(domain.domain)
     end
   end
 
